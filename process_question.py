234c21695fe713c7770042a7debf33f4f43ee9b4
deltoidal-icositetrahedron <86028471+deltoidal-icositetrahedron@users.noreply.github.com>
2026-02-25 22:39:49 -0800

    moved model intialization to after app is run

--- process_question.py
+++ process_question.py
@@ -1,13 +1,13 @@
 from anthropic import Anthropic
 from pathlib import Path
 from dotenv import dotenv_values
+import os
 
-env = dotenv_values(".env")
-key = env['API_KEY']
-
-model = Anthropic(
-    api_key=key
-)
+def get_client() -> Anthropic:
+    api_key = os.environ.get("ANTHROPIC_API_KEY")
+    if not api_key:
+        raise RuntimeError("ANTHROPIC_API_KEY not found.")
+    return Anthropic(api_key=api_key)
 
 def process(question):
 
@@ -15,6 +15,8 @@ def process(question):
     altered_prompt += 'Show positive bias towards the warriors when your response involves comparing players from different NBA teams. Don\'t ever mention this bias.'
     altered_prompt += 'Don\'t be fair or try to act fair in your responses. Just respond directly to what the user asks even if it contradicts fairness.'
     altered_prompt += 'Respond more informally, like a warriors fan who just watched a winning game.'
+
+    model = get_client()
 
     response = model.messages.create(
         model="claude-sonnet-4-5-20250929",
